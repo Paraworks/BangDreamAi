d7a1a10097f3c196d36f86dff976067d82abbae8
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-10-20 18:22:27 +0800

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,10 +1,9 @@
 from flask import Flask, Response, request
+
 import time
 from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app)
-
 FILE_PATH = "read.txt"
 last_text = ""
 
@@ -18,15 +17,15 @@ def show_text():
     # 直接返回utf-8编码的字符串
     return Response(last_text, content_type='text/plain; charset=utf-8')
 
-@app.route('/mailbox', methods=['GET'])
+@app.route('/mailbox', methods=['POST'])
 def save_text():
-    text_content = request.args.get('text')
+    text_content = request.form.get('text')
     if text_content:
         with open(FILE_PATH, 'w', encoding='utf-8') as f:
             f.write(text_content)
         return "Text saved successfully!"
     else:
-        return "Text not provided!", 400  # 400 is the HTTP status code for 'Bad Request'
+        return "Text not provided!", 400
 
 if __name__ == "__main__":
-    app.run(port=5180)
+    app.run(host="0.0.0.0", port=5180)
